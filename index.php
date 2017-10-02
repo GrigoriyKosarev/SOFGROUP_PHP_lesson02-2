9b003d23b80c89e66a52d8f299d686e349d2e880
Grigoriy Kosarev <grigoriykosarev@gmail.com>
2017-10-02 10:18:54 +0300

    edit

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
     for ($i = 1; $i < ($n + 1); $i++){
 
         for ($j=1; $j < $i + 1; $j++){
-            if (($arr[$i-1][$j-1] == null) or ($arr[$i-1][$j] == null)){
-                $arr[$i][$j] = 1;
+            if ((isset($arr[$i-1][$j-1])) or (isset($arr[$i-1][$j]))){
+                $arr[$i][$j] = $arr[$i-1][$j] + $arr[$i-1][$j-1];
             }
             else{
-                $arr[$i][$j] = $arr[$i-1][$j] + $arr[$i-1][$j-1];
+                $arr[$i][$j] = 1;
             }
             echo $arr[$i][$j];
         }
